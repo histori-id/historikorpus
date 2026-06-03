9e737df1abdcc7ee39b186686c7df0418285e0ef
Nabil Muhammad Azzam <40307963+nabilazzam@users.noreply.github.com>
2026-06-03 10:38:40 +0700

    added ocr dependencies

---
+++ install_ocr_dependencies.sh
@@ -0,0 +1,7 @@
+!pip install doclayout-yolo
+!sudo apt install tesseract-ocr
+!pip install pytesseract
+!apt-get install -y tesseract-ocr
+!apt-get install -y libtesseract-dev
+!sudo apt install tesseract-ocr-ind
+!sudo apt install tesseract-ocr-nld
